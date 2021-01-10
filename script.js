f48efa8e1487a2a147e75071fd08215c6bda3f3d
Manjinder <manjindersinghjohal@aol.com>
2021-01-11 00:46:02 +0530

    added styles

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let celcius;
 const convert = () => {
  celcius = document.getElementById('celcius').value;
  let fahrenheit = (celcius * 9/5) + 32; 
- document.getElementById('fahrenheit').innerHTML = fahrenheit;
+ document.getElementById('fahrenheit').innerHTML = `Fahrenheit is ${fahrenheit}`;
 }
