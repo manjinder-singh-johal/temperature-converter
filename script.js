82e9426e61f40d8ed91f2f10a46c79abca70115d
Manjinder <manjindersinghjohal@aol.com>
2021-01-11 00:23:27 +0530

    added conversion funcationality

---
+++ script.js
@@ -0,0 +1,7 @@
+let celcius;
+
+const convert = () => {
+ celcius = document.getElementById('celcius').value;
+ let fahrenheit = (celcius * 9/5) + 32; 
+ document.getElementById('fahrenheit').innerHTML = fahrenheit;
+}
